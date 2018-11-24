304e9083500d38081765a90a4ee4ab9c4bba04fe
colabread <642020701@qq.com>
2018-11-24 12:02:25 +0800

    添加日志

--- src/main/java/com/aidilude/demo/controller/BaseController.java
+++ src/main/java/com/aidilude/demo/controller/BaseController.java
@@ -2,9 +2,13 @@ package com.aidilude.demo.controller;
 
 import com.aidilude.demo.util.BlockUtils;
 import com.aidilude.demo.util.StringUtils;
+import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/base")
@@ -18,6 +22,19 @@ public class BaseController {
             return "503";
         BlockUtils.reset();
         return "666";
+    }
+
+    @GetMapping("/queryBlockData")
+    public String queryBlockData(String secret){
+        if(StringUtils.isEmpty(secret))
+            return "403";
+        if(!secret.equals("mxb44hEQHxlkdl7aYnTMCvF5zTuoQyEZet3tGobU6VfemON4EM05RGtRdFw7TjCZ"))
+            return "503";
+        Map<String, Object> result = new HashMap<>();
+        result.put("blockHeight", BlockUtils.getBlockHeight());
+        result.put("exceptionCount", BlockUtils.getExceptionCount());
+        result.put("sendMsgCount", BlockUtils.getSendMsgCount());
+        return JSON.toJSONString(result);
     }
 
 }
--- src/main/java/com/aidilude/demo/task/BlockCheckTask.java
+++ src/main/java/com/aidilude/demo/task/BlockCheckTask.java
@@ -17,6 +17,7 @@ public class BlockCheckTask {
 
     @Scheduled(cron = "0 */1 * * * ?")   //每分钟执行一次
     public void run(){
+        log.info("【区块检查任务】开始执行");
         String result = null;
         try {
             result = HttpUtils.doGet("http://47.244.98.189:8192/api/chains/ASCHBet/blocks/height", null);
@@ -27,34 +28,41 @@ public class BlockCheckTask {
             log.error("【区块检查任务】调用区块接口异常");
         JSONObject jsonResult = JSONObject.parseObject(result);
         if(jsonResult.getBooleanValue("success") != true){
+            log.error("【区块检查任务】success：false，发送短信警告");
             sendMsg("777777");
         }else{
             Integer nowBlockHeight = jsonResult.getInteger("height");
             if(BlockUtils.getBlockHeight() == 0){
+                log.info("【区块检查任务】初始化区块高度");
                 BlockUtils.setBlockHeight(nowBlockHeight);
             }else{
                 if(BlockUtils.getBlockHeight() == nowBlockHeight){   // --> 区块高度不增加，区块异常
                     if(BlockUtils.getExceptionCount() >= 3){   // --> 异常次数超过三次
                         if(BlockUtils.getSendMsgCount() < 3){   // --> 短信通知次数小于3次，发送短信警告
+                            log.error("【区块检查任务】区块异常，次数超过三次，发送短信警告");
                             sendMsg("666666");
                             BlockUtils.setSendMsgCount(BlockUtils.getSendMsgCount() + 1);
                         }else {
-                            log.error("【区块检查任务】区块高度不增加，短信次数超过三次");
+                            log.error("【区块检查任务】区块异常，短信次数超过三次");
                         }
                     }else{
+                        log.error("【区块检查任务】区块异常，更新异常次数");
                         BlockUtils.setExceptionCount(BlockUtils.getExceptionCount() + 1);
                     }
                 }else{
+                    log.info("【区块检查任务】区块正常，更新高度");
                     BlockUtils.setBlockHeight(nowBlockHeight);
                 }
             }
         }
+        log.info("【区块检查任务】执行结束");
+        log.info("**************************************************************************");
     }
 
     public void sendMsg(String msg){
-        SMUtils.sendMsg(msg, SMUtils.phoneTang);
-        SMUtils.sendMsg(msg, SMUtils.phoneYang);
-        SMUtils.sendMsg(msg, SMUtils.phoneDu);
+        log.info("【区块检查任务】发送短信结果：" + SMUtils.sendMsg(msg, SMUtils.phoneTang));
+        log.info("【区块检查任务】发送短信结果：" + SMUtils.sendMsg(msg, SMUtils.phoneYang));
+        log.info("【区块检查任务】发送短信结果：" + SMUtils.sendMsg(msg, SMUtils.phoneDu));
     }
 
 }
